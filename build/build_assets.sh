97bbdb7ed31f4f91084ef4aaccf4222747b53ba7
Daniel Pierce <dlpierce@iu.edu>
2025-01-30 13:56:42 -0500

    Use nulldb for precompile

--- build/build_assets.sh
+++ build/build_assets.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 if [ "${RAILS_ENV}" = 'production' ] || [ "${RAILS_ENV}" = 'staging' ]; then
-  SECRET_KEY_BASE=TEMPORARY_SECRET_KEY VALKYRIE_SOLR_HOST=localhost VALKYRIE_SOLR_PORT=8983 VALKYRIE_SOLR_CORE=pg bundle exec rails assets:precompile
+  SECRET_KEY_BASE=TEMPORARY_SECRET_KEY DATABASE_URL='nulldb://nulldb' VALKYRIE_SOLR_HOST=localhost VALKYRIE_SOLR_PORT=8983 VALKYRIE_SOLR_CORE=pg bundle exec rails assets:precompile
 fi
