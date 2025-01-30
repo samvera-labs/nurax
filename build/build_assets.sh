4ab99c6536260fb89a868b1142254f8dbb9c7f83
Daniel Pierce <dlpierce@iu.edu>
2025-01-30 13:55:31 -0500

    Use nulldb for precompile

--- build/build_assets.sh
+++ build/build_assets.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 if [ "${RAILS_ENV}" = 'production' ] || [ "${RAILS_ENV}" = 'staging' ]; then
-  SECRET_KEY_BASE=TEMPORARY_SECRET_KEY bundle exec rails assets:precompile
+  SECRET_KEY_BASE=TEMPORARY_SECRET_KEY DATABASE_URL='nulldb://nulldb' bundle exec rails assets:precompile
 fi
